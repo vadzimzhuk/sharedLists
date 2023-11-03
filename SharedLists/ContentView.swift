eddc58f59c3f92254fa6a7953cfa6d25c14b697d
AppRoach <codeapproach@gmail.com>
2023-11-03 14:56:03 +0100

    Initial Commit

---
+++ SharedLists/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SharedLists
+//
+//  Created by Vadim Zhuk on 03/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SharedLists/SharedListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedListsApp.swift
+//  SharedLists
+//
+//  Created by Vadim Zhuk on 03/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SharedListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
