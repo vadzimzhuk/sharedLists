71c5dfe8d3607f5493a6aa2926c86d8da09eb818
AppRoach <codeapproach@gmail.com>
2023-11-10 13:14:06 +0100

    Import shared list deeplink implemented

--- SharedLists/SharedListsApp.swift
+++ SharedLists/SharedListsApp.swift
@@ -12,6 +12,7 @@ import FirebaseCore
 struct SharedListsApp: App {
 
     private let store: AppStateStore
+    private let storage: FirestoreService
 
     init() {
         DIContainer.shared.registerDependencies()
@@ -20,15 +21,59 @@ struct SharedListsApp: App {
 
         FirebaseApp.configure()
 
-        let storage: FirestoreService = DIContainer.shared.resolve(FirestoreService.self)!
+        storage = DIContainer.shared.resolve(FirestoreService.self)!
         store.register(middleware: authorizeMiddleware(storage: storage))
     }
 
+    @State var importListAlertShown: Bool = false
+    @State var importListData: (userId: String, listId: String)? = nil
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(store)
+                .onOpenURL { url in
+                    handle(url: url)
+                }
+                .alert("Import external list", isPresented: $importListAlertShown) {
+                    Button("Import", role: .none) {
+                        guard let importListData else { assertionFailure(); return }
+                        
+                        Task {
+                            await storage.addExternalList(with: importListData.listId, of: importListData.userId)
+                        }
+                    }
+                } message: {
+                    Text("Do you really want to add shared list")
+                }
+
         }
+    }
+}
+
+extension SharedListsApp {
+    enum RemoteAction: String {
+        case sharelist
+        case unhandled
+    }
+
+    private func handle(url: URL) {
+        let actionData = parseData(from: url)
+        // check if the user isnt the current one
+        importListData = (actionData.userId, actionData.listId)
+        importListAlertShown = true
+    }
+
+    private func parseData(from url: URL) -> (command: RemoteAction, userId: String, listId: String) {
+        let pathComponents = url.pathComponents
+        let components = NSURLComponents(url: url, resolvingAgainstBaseURL: false)
+        let query = components?.queryItems
+
+        let listId = query?.first { $0.name == "listid" }?.value ?? ""
+        let userId = query?.first { $0.name == "userid" }?.value ?? ""
+
+        let command = RemoteAction(rawValue: String(url.host()?.split(separator: ".").first ?? "")) ?? .unhandled
+
+        return (command: command, userId: userId, listId: listId)
     }
 }
